b25e213b783fca4c2dfc2d79e570d0779627b948
augustobarzola <83320894+augustobarzola@users.noreply.github.com>
2023-06-01 21:05:50 -0300

    Practico0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,4 @@
+let base = prompt('Ingrese base del rectangulo');
+let altura = prompt('Ingrese altura del rectangulo');
+var area = base * altura;
+console.log('El area del rectangulo es: ' + area);
---
+++ practico0/promedio3.js
@@ -0,0 +1,5 @@
+let num1 = prompt("Ingrese el valor del primer numero: ")
+let num2 = prompt("Ingrese el valor del segundo numero: ")
+let num3 = prompt("Ingrese el valor del tercero numero: ")
+var promedio = (parseInt(num1) + parseInt(num2) + parseInt(num3)) / 3
+console.log("El valor del promedio es: " + parseFloat(promedio));
