e0323f42daeb3b7b3bd931bd757a4cfd09a07e6a
augustobarzola <83320894+augustobarzola@users.noreply.github.com>
2023-06-01 21:03:13 -0300

    Se agregan los archivos de resta y multiplicacion

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,5 @@
+let num1 = prompt("Ingrese el valor del primer numero")
+let num2 = prompt("Ingrese el valor del segundo numero")
+let num3 = prompt("Ingrese el valor del tercer numero")
+var producto = num1 * num2 * num3
+console.log("El resultado del producto de los tres numeros es: " + producto)
---
+++ practico0/resta.js
@@ -0,0 +1,4 @@
+let min = prompt('Ingrese el primer numero');
+let sus = prompt('Ingrese el segundo numero');
+var resta = min - sus;
+console.log('El resultado de la resta es: ' + resta);
